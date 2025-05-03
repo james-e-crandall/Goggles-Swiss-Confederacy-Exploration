e44747d818172562fa333e0ca1bb680c99f6b531
james-e-crandall <james.e.crandall@gmail.com>
2025-05-02 21:43:03 -0500

    visual remap, portrait remap

---
+++ modules/ui/game.js
@@ -0,0 +1,58 @@
+window.UnitPortraitVisualRemaps = {
+    ...(window.UnitPortraitVisualRemaps || {}),
+    'UNIT_PIKE_SQUARE': { //your unit type
+        unitType: 'UNIT_PIKEMAN',
+        background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
+    },
+    'UNIT_PIKE_SQUARE_2': { //your unit type
+        unitType: 'UNIT_PIKEMAN',
+        background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
+    },
+    'UNIT_PIKE_SQUARE_3': { //your unit type
+        unitType: 'UNIT_PIKEMAN',
+        background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
+    },
+    'UNIT_AMTSBURGERMEISTER_JOHANN_CONRAD_FISCHER': { //your unit type
+        unitType: 'UNIT_HUSSAR',
+        background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
+    },
+    'UNIT_AMTSBURGERMEISTER_JOACHIM_VADIAN': { //your unit type
+        unitType: 'UNIT_HUSSAR',
+        background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
+    },  
+    'UNIT_AMTSBURGERMEISTER_WILHELM_HERTER_VON_HERTNECK': { //your unit type
+        unitType: 'UNIT_HUSSAR',
+        background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
+    },
+    'UNIT_AMTSBURGERMEISTER_MARX_ROIST': { //your unit type
+        unitType: 'UNIT_HUSSAR',
+        background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
+    },    
+    'UNIT_AMTSBURGERMEISTER_ELISABTH_OF_WETZIKON': { //your unit type
+        unitType: 'UNIT_HUSSAR',
+        background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
+    },      
+    'UNIT_AMTSBURGERMEISTER_HULDRYCH_ZEINGLI': { //your unit type
+        unitType: 'UNIT_HUSSAR',
+        background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
+    }, 
+    'UNIT_LEHRBEAUFTRAGTER_LEONHARD_EULER': { //your unit type
+        unitType: 'UNIT_HUSSAR',
+        background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
+    },   
+    'UNIT_LEHRBEAUFTRAGTER_DANIEL_BERNOULLI': { //your unit type
+        unitType: 'UNIT_HUSSAR',
+        background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
+    },   
+    'UNIT_LEHRBEAUFTRAGTER_PARACELSUS': { //your unit type
+        unitType: 'UNIT_HUSSAR',
+        background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
+    },  
+
+}
+
+
+
+
+
+
